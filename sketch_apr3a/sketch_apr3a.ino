1fd3661aaec055d21a9b31337a4573194c7d0f1b
DanKubei <tirran45@yandex.ru>
2024-04-10 13:06:43 +0300

    Some strange update from telegram

--- sketch_apr3a/sketch_apr3a.ino
+++ sketch_apr3a/sketch_apr3a.ino
@@ -1,42 +1,44 @@
 // кнопки наше всё
 // сделать UART те же 0 и 1 
+// Использовать энеогосберегающую память запомненные позиции
+
 #include "GyverStepper.h"
+#include <EEPROM.h>
 //#include "GyverPlanner.h"
-#define sensorMax0 30
-#define sensorMax1 31
-#define sensorMax2 32
-#define sensorMax3 33
+
+#define sensorMax1 30
+#define sensorMax2 31
+#define sensorMax3 32
+#define sensorMax4 33
 #define catcher 2
-#define buttonRotationPlusA0 11
-#define buttonRotationMinusA0 12
-#define buttonRotationPlusA1 13
-#define buttonRotationMinusA1 14
-#define buttonRotationPlusA2 15
-#define buttonRotationMinusA2 16
-#define buttonRotationPlusA3 17
-#define buttonRotationMinusA3 18
+#define buttonRotationPlusA1 11
+#define buttonRotationMinusA1 12
+#define buttonRotationPlusA2 13
+#define buttonRotationMinusA2 14
+#define buttonRotationPlusA3 15
+#define buttonRotationMinusA3 16
+#define buttonRotationPlusA4 17
+#define buttonRotationMinusA4 18
 #define UART 19
+
+
 
 int pins[4][3] = {
   { 0, 1, 6 },
   { 2, 3, 7 },
   { 3, 4, 9 },
-  { 4, 5, 10 },
+  { 11, 5, 10 },
 };  // {{dir,en,step}}
 long positionsDrivers[4] = { 0, 0, 0, 0 };
-int coordinatesProgram[5][4]{
-  { 0, 0, 0, 0 },
-  { 3453, 2353, 5345, -5424 },
-  { 4654, -4658, 7825, 9972 },
-  { 1024, 2048, -3060, 1 },
-  { -2342, 2, 4234, 8768 },
-};
-
-int coordinates[4]={0,0,0,0};
 
 int counterCoordinates = 0;
 
+
+
+
 int readyDrivers = 0;
+
+
 
 GStepper<STEPPER2WIRE> stepper1(8192, pins[0][0], pins[0][0], pins[0][1]);
 GStepper<STEPPER2WIRE> stepper2(8192, pins[1][3], pins[1][0], pins[1][1]);
@@ -63,6 +65,7 @@ void setup() {
   stepper4.setAcceleration(300);
   stepper4.enable();
   stepper4.setRunMode(FOLLOW_POS);
+  
   /*planner.addStepper(0,stepper1);
   planner.addStepper(1,stepper2);
   planner.addStepper(2,stepper3);
@@ -74,6 +77,12 @@ void setup() {
 }
 
 void loop() {
+  if (startMove == true){
+    rotationAuto();
+  }
+  else {
+    rotation();
+  }
   
 }
 
@@ -82,66 +91,69 @@ void loop() {
 
 // ручное
 void rotation() {
-  if (digitalRead(buttonRotationPlusA0) == HIGH) {
+  int buttonsActive = (digitalRead(buttonRotationPlusA1) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationMinusA1) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationPlusA2) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationMinusA2) == HIGH) ? 1 : 0 
+    + (digitalRead(buttonRotationPlusA3) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationMinusA3) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationPlusA4) == HIGH) ? 1 : 0
+    + (digitalRead(buttonRotationMinusA4) == HIGH) ? 1 : 0;
+  if (buttonsActive > 1) {
+    return;
+  }
+  if (digitalRead(buttonRotationPlusA1) == HIGH) {
     stepper1.reverse(false);
     stepper1.setTarget(1, RELATIVE);
   }
-  if (digitalRead(buttonRotationMinusA0) == HIGH) {
+  if (digitalRead(buttonRotationMinusA1) == HIGH) {
     stepper1.reverse(true);
     stepper1.setTarget(1, RELATIVE);
   }
-  if (digitalRead(buttonRotationPlusA1) == HIGH) {
+  if (digitalRead(buttonRotationPlusA2) == HIGH) {
     stepper2.reverse(false);
     stepper2.setTarget(1, RELATIVE);
   }
-  if (digitalRead(buttonRotationMinusA1) == HIGH) {
+  if (digitalRead(buttonRotationMinusA2) == HIGH) {
     stepper2.reverse(true);
     stepper2.setTarget(1, RELATIVE);
   }
- if (digitalRead(buttonRotationPlusA2) == HIGH) {
+ if (digitalRead(buttonRotationPlusA3) == HIGH) {
     stepper3.reverse(false);
     stepper3.setTarget(1, RELATIVE);
   }
-  if (digitalRead(buttonRotationMinusA2) == HIGH) {
+  if (digitalRead(buttonRotationMinusA3) == HIGH) {
     stepper3.reverse(true);
     stepper3.setTarget(1, RELATIVE);
   }
- if (digitalRead(buttonRotationPlusA3) == HIGH) {
+ if (digitalRead(buttonRotationPlusA4) == HIGH) {
     stepper4.reverse(false);
     stepper4.setTarget(1, RELATIVE);
   }
-  if (digitalRead(buttonRotationMinusA3) == HIGH) {
+  if (digitalRead(buttonRotationMinusA4) == HIGH) {
     stepper4.reverse(true);
     stepper4.setTarget(1, RELATIVE);
   }
-  positionsDrivers[0]=stepper1.getTarget();
-  positionsDrivers[1]=stepper1.getTarget();
-  positionsDrivers[2]=stepper1.getTarget();
-  positionsDrivers[3]=stepper1.getTarget();
 }
 
 //работа через запарашенные коорды
+int rotationIndex = 0;
 void rotationAUTO() {
-  if (!stepper1.tick()) {
-    stepper1.setTarget(coordinates[0], ABSOLUTE);
-    readyDrivers += 1;
-    positionsDrivers[0] = stepper1.getCurrent();
-  } else if (!stepper2.tick()) {
-    stepper2.setTarget(coordinates[1], ABSOLUTE);
-    readyDrivers += 1;
-    positionsDrivers[1] = stepper2.getCurrent();
-  } else if (!stepper3.tick()) {
-    stepper3.setTarget(coordinates[2], ABSOLUTE);
-    readyDrivers += 1;
-    positionsDrivers[2] = stepper3.getCurrent();
-  } else if (!stepper4.tick()) {
-    stepper4.setTarget(coordinates[3], ABSOLUTE);
-    readyDrivers += 1;
-    positionsDrivers[3] = stepper4.getCurrent();
+  if (rotationIndex == 4){
+    rotationIndex = 0;
   }
-  if (readyDrivers == 4) {
-    counterCoordinates += 1;
-    readyDrivers = 0;
+  if (rotationIndex == 0) {
+      stepper1.setTarget(coordinates[0], ABSOLUTE);
+      rotationIndex++;
+  } else if (rotationIndex == 1 && !stepper1.tick() ) {
+      stepper2.setTarget(coordinates[1], ABSOLUTE);
+      rotationIndex++;
+  } else if (rotationIndex == 2 && !stepper2.tick()) {
+      stepper3.setTarget(coordinates[2], ABSOLUTE);
+      rotationIndex++;
+  } else if (rotationIndex == 3 && !stepper3.tick()) {
+      stepper4.setTarget(coordinates[3], ABSOLUTE);
+      rotationIndex++;
   }
 }
 
@@ -153,34 +165,58 @@ void Program(){
 
 
 void moveToHome() {
-  while (digitalRead(sensorMax1) == LOW) {
-
-  }
-}
-
-int counterDrivers = 0;
-int counterBit = 0;
-int timer=0;
-bool parsing = false;
-
-void parseUART(){
-  if (digitalRead(UART)==HIGH && parsing == false){
-    timer = millis();
-    parsing = true;
-    counterDrivers = 0;
-  }
-  else if (millis()-timer > 1000) {
-    if (digitalRead(UART) == HIGH){
-      coordinates[counterDrivers] += 2^counterBit;
+  while (true) {
+    if (digitalRead(sensorMax1) == 0) {
+        stepper1.reverse(true);
+        stepper1.setTarget(1, RELATIVE);
     }
-    counterBit +=1;
-    timer = millis();
-    if (counterBit == 8){
-      counterDrivers+=1;
+    else if (digitalRead(sensorMax2) == 0) {
+        stepper2.reverse(true);
+        stepper2.setTarget(1, RELATIVE);
     }
-    if (counterDrivers == 4){
-      parsing = false;
-      counterDrivers = 0;      
+    else if (digitalRead(sensorMax3) == 0) {
+        stepper3.reverse(true);
+        stepper3.setTarget(1, RELATIVE);
+    }
+    else if (digitalRead(sensorMax1) == 1 && digitalRead(sensorMax2) == 1 && digitalRead(sensorMax3) == 1 ) {
+      stepper1.setCurrent(0);
+      stepper2.setCurrent(0);
+      stepper3.setCurrent(0);
+      break;
     }
   }
 }
+
+
+
+    // число значений в массиве, который хотим получить
+int parsingData[257] = new int [257];     // массив численных значений после парсинга
+boolean recievedFlag = false;
+boolean getStarted = false;
+byte index = 0;
+String string_convert = "";
+
+void parsing() {
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();        // обязательно ЧИТАЕМ входящий символ
+    if (getStarted) {                         // если приняли начальный символ (парсинг разрешён)
+      if (incomingByte != ' ' && incomingByte != ';') {   // если это не пробел И не конец
+        string_convert += incomingByte;       // складываем в строку
+      } else {                                // если это пробел или ; конец пакета
+        parsingData[index] = string_convert.toInt();  // преобразуем строку в int и кладём в массив
+        string_convert = "";                  // очищаем строку
+        index++;                              // переходим к парсингу следующего элемента массива
+      }
+    }
+    if (incomingByte == '$') {                // если это $
+      getStarted = true;                      // поднимаем флаг, что можно парсить
+      index = 0;                              // сбрасываем индекс
+      string_convert = "";                    // очищаем строку
+    }
+    if (incomingByte == ';') {                // если таки приняли ; - конец парсинга
+      getStarted = false;                     // сброс
+      recievedFlag = true;                    // флаг на принятие
+    }
+  }
+}
+  
